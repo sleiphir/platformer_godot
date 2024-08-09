2a8ff95f43b87b5ba026fdf3a8dac7bb9887ae7e
Simon Lasbrugnas <simon.lasbrugnas@gmail.com>
2024-08-09 17:38:53 +0200

    update

--- scripts/main.gd
+++ scripts/main.gd
@@ -8,4 +8,4 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	$SnowParticles.position.x = $Character.position.x
--- scripts/platforms/chained_platform.gd
+++ scripts/platforms/chained_platform.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
+const INITIAL_FORCE_APPLIED = 500 # the amount of force applied initally to the platform to make it move
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	platform_impulse(Vector2(INITIAL_FORCE_APPLIED * randf_range(-1,1), 0))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
